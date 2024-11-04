4c1ebec996cff49da72030c9ccb84b2a0da75bab
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-11-04 20:37:03 -0300

    docs: creating documentation from BookController.php

--- app/Http/Controllers/Api/V1/Book/BookController.php
+++ app/Http/Controllers/Api/V1/Book/BookController.php
@@ -7,23 +7,44 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\Books\BookResource;
 use App\Models\Book;
 use App\Traits\HttpResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * Class BookController
+ * @package App\Http\Controllers\Api\V1\Book
+ * @group Book
+ * @authenticated
+ * @resource Book
+ * @apiResourceCollection App\Http\Resources\Api\V1\Books\BookResource
+ * @apiResource App\Models\Book
+ * Controller que manipular informações dos livros
+ */
 class BookController extends Controller
 {
     use HttpResponse;
 
-
+    /**
+     * @return JsonResponse
+     * Busca todos os livros
+     */
     public function index()
     {
         return $this->response('Livros', 200, BookResource::collection(Book::all()));
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return JsonResponse
+     * Cadastra um livro
+     *
+     *  Parâmetros esperados no corpo da requisição:
+     *  - title (required|string): Título do livro.
+     *  - author (required|string): Autor do livro.
+     *  - description (required|string): Descrição do livro.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
 
         $validation = Validator::make($request->all(), [
@@ -43,7 +64,9 @@ class BookController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * @param Book $book
+     * @return JsonResponse
+     * Busca um livro, passando o id do livro
      */
     public function show(Book $book)
     {
@@ -51,10 +74,22 @@ class BookController extends Controller
 
     }
 
+
     /**
-     * Update the specified resource in storage.
+     * Atualiza um livro existente buscando-o pelo código.
+     *
+     *
+     * Parâmetros esperados no corpo da requisição:
+     * - title (nullable|string): Título do livro (opcional).
+     * - author (nullable|string): Autor do livro (opcional).
+     * - description (nullable|string): Descrição do livro (opcional).
+     *
+     * @param Request $request Dados da requisição, incluindo os campos do livro a serem atualizados.
+     * @param Book $book Livro a ser atualizado.
+     * @return JsonResponse Retorna uma resposta JSON com uma mensagem de sucesso e o recurso atualizado,
+     * ou uma mensagem de erro com os detalhes da validação.
      */
-    public function update(Request $request, Book $book)
+    public function update(Request $request, Book $book): JsonResponse
     {
         $validation = Validator::make($request->all(), [
             'title' => 'nullable|string',
@@ -67,12 +102,13 @@ class BookController extends Controller
         }
 
         $book->update($validation->validated());
-        return $this->response('livro atualizado com sucesso', 200, new BookResource($book));
-
+        return $this->response('Livro atualizado com sucesso', 200, new BookResource($book));
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @param Book $book
+     * @return JsonResponse
+     * Deleta um livro
      */
     public function destroy(Book $book)
     {
