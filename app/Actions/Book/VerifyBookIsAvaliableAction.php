78ebbf6eca4625752e22af37186a1e726414e564
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-11-04 21:03:41 -0300

    docs: creating documentation from VerifyBookIsAvaliableAction.php

--- app/Actions/Book/VerifyBookIsAvaliableAction.php
+++ app/Actions/Book/VerifyBookIsAvaliableAction.php
@@ -4,17 +4,25 @@ namespace App\Actions\Book;
 
 use App\Enums\BookStatus;
 use App\Models\Book;
+use BadMethodCallException;
 
 class VerifyBookIsAvaliableAction
 {
 
-    public function execute(Book $book)
-    {
-        if (!$book->status) {
-            throw new \BadMethodCallException('O livro não tem o campo status');
-        }
-        return $book->status === BookStatus::Available->value;
+/**
+ * Verifica se um livro está disponível.
+ *
+ * @param Book $book O livro a ser verificado.
+ * @return bool Retorna true se o livro estiver disponível e false caso contrário.
+ * @throws BadMethodCallException Se o livro não tiver o campo status.
+ */
+public function execute(Book $book): bool
+{
+    if (!$book->status) {
+        throw new BadMethodCallException('O livro não tem o campo status');
     }
+    return $book->status === BookStatus::Available->value;
+}
 
 
 }
