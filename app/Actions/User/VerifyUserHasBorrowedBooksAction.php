4d8b70d8c36de02ed6c28a923f337ab69d2addd1
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-11-04 21:05:56 -0300

    docs: creating documentation from VerifyUserHasBorrowedBooksAction.php

--- app/Actions/User/VerifyUserHasBorrowedBooksAction.php
+++ app/Actions/User/VerifyUserHasBorrowedBooksAction.php
@@ -3,10 +3,25 @@
 namespace App\Actions\User;
 
 use App\Models\User;
+use BadMethodCallException;
 
+/**
+ * Class VerifyUserHasBorrowedBooksAction
+ *
+ * Essa classe verifica se o usuário tem livros emprestados .
+ *
+ * @package App\Actions\User
+ */
 class VerifyUserHasBorrowedBooksAction
 {
-    public function execute(User $user)
+    /**
+     * verifica se o usuário tem livros emprestados.
+     *
+     * @param User $user O usuário a ser verificado.
+     * @return bool Retorna verdadeiro se o usuário tiver livros emprestados, falso caso contrário.
+     * @throws BadMethodCallException Se o usuário não tiver o campo `has_borrowed_books`.
+     */
+    public function execute(User $user): bool
     {
         if (!isset($user->has_borrowed_books)) {
             throw new \BadMethodCallException('O usuário não possui o campo `has_borrowed_books`');
