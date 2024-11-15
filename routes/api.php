68d44bcc6575b9224c9681fab6cab4e0abef383a
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-11-15 17:09:26 -0300

    refactor: criando um grupo de rotas com o middleware de auth e separando rotas de usuário

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\Auth\Authenticate;
 
@@ -11,45 +10,59 @@ Route::prefix('v1')->group(function () {
         return response()->json(['message' => 'pong']);
     });
 
+
+    Route::post('register', [App\Http\Controllers\Api\V1\User\UserController::class, 'store'])
+        ->name('register');
+
     Route::post('login', [App\Http\Controllers\Api\V1\Auth\LoginController::class, '__invoke'])
         ->name('login');
 
-    Route::post('logout', [App\Http\Controllers\Api\V1\Auth\LogoutController::class, '__invoke'])
-        ->name('logout')->middleware(Authenticate::class);
+    Route::group(['middleware' => Authenticate::class], function () {
 
-    Route::apiResource('books', App\Http\Controllers\Api\V1\Book\BookController::class)
-        ->names(
-            [
-                'index' => 'books.index',
-                'store' => 'books.store',
-                'show' => 'books.show',
-                'update' => 'books.update',
-                'destroy' => 'books.destroy',
-            ]
-        )->middleware(Authenticate::class);
+        Route::post('logout', [App\Http\Controllers\Api\V1\Auth\LogoutController::class, '__invoke'])
+            ->name('logout');
 
-    Route::apiResource('loans', App\Http\Controllers\Api\V1\Loan\LoanController::class)
-        ->names(
-            [
-                'index' => 'loans.index',
-                'store' => 'loans.store',
-                'show' => 'loans.show',
-                'update' => 'loans.update',
-                'destroy' => 'loans.destroy',
-            ]
-        )->middleware(Authenticate::class);
+        Route::apiResource('books', App\Http\Controllers\Api\V1\Book\BookController::class)
+            ->names(
+                [
+                    'index' => 'books.index',
+                    'store' => 'books.store',
+                    'show' => 'books.show',
+                    'update' => 'books.update',
+                    'destroy' => 'books.destroy',
+                ]
+            );
 
-    Route::post('loans/{loan}/devolution', [App\Http\Controllers\Api\V1\Loan\LoanDevolutionController::class, '__invoke'])
-        ->name('loans.devolution')->middleware(Authenticate::class);
+        Route::apiResource('loans', App\Http\Controllers\Api\V1\Loan\LoanController::class)
+            ->names(
+                [
+                    'index' => 'loans.index',
+                    'store' => 'loans.store',
+                    'show' => 'loans.show',
+                    'update' => 'loans.update',
+                    'destroy' => 'loans.destroy',
+                ]
+            );
 
-    Route::apiResource('users', App\Http\Controllers\Api\V1\User\UserController::class)
-        ->names(
-            [
-                'index' => 'users.index',
-                'store' => 'users.store',
-                'show' => 'users.show',
-                'update' => 'users.update',
-                'destroy' => 'users.destroy',
-            ]
-        )->middleware(Authenticate::class);
+        Route::post('loans/{loan}/devolution', [App\Http\Controllers\Api\V1\Loan\LoanDevolutionController::class, '__invoke'])
+            ->name('loans.devolution');
+
+        Route::prefix('users')->group(function () {
+            Route::get('/', [App\Http\Controllers\Api\V1\User\UserController::class, 'index'])
+                ->name('users.index');
+
+            Route::get('/{user}', [App\Http\Controllers\Api\V1\User\UserController::class, 'show'])
+                ->name('users.show');
+
+            Route::put('/{user}', [App\Http\Controllers\Api\V1\User\UserController::class, 'update'])
+                ->name('users.update');
+
+            Route::delete('/{user}', [App\Http\Controllers\Api\V1\User\UserController::class, 'destroy'])
+                ->name('users.destroy');
+        });
+
+    });
+
+
 });
+
