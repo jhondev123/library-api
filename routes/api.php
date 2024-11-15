8108ad7bcf1b07380ec2500b9b2f0e1c541ffe51
jhondev123 <jhonattan.curtarelli@outlook.com>
2024-11-15 17:58:56 -0300

    refactor: Refatorando código na api.php

--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,32 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\Auth\Authenticate;
 
+use App\Http\Controllers\Api\V1\User\UserController;
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
+use App\Http\Controllers\Api\V1\Book\BookController;
+use App\Http\Controllers\Api\V1\Loan\LoanController;
+use App\Http\Controllers\Api\V1\Loan\LoanDevolutionController;
 Route::prefix('v1')->group(function () {
 
-    Route::get('/ping', function () {
+    Route::get('/ping', static function () {
         return response()->json(['message' => 'pong']);
     });
 
 
-    Route::post('register', [App\Http\Controllers\Api\V1\User\UserController::class, 'store'])
+    Route::post('register', [UserController::class, 'store'])
         ->name('register');
 
-    Route::post('login', [App\Http\Controllers\Api\V1\Auth\LoginController::class, '__invoke'])
+    Route::post('login', [LoginController::class, '__invoke'])
         ->name('login');
 
-    Route::group(['middleware' => Authenticate::class], function () {
+    Route::group(['middleware' => Authenticate::class], static function () {
 
-        Route::post('logout', [App\Http\Controllers\Api\V1\Auth\LogoutController::class, '__invoke'])
+        Route::post('logout', [LogoutController::class, '__invoke'])
             ->name('logout');
 
-        Route::apiResource('books', App\Http\Controllers\Api\V1\Book\BookController::class)
+        Route::apiResource('books', BookController::class)
             ->names(
                 [
                     'index' => 'books.index',
@@ -33,7 +39,7 @@ Route::prefix('v1')->group(function () {
                 ]
             );
 
-        Route::apiResource('loans', App\Http\Controllers\Api\V1\Loan\LoanController::class)
+        Route::apiResource('loans', LoanController::class)
             ->names(
                 [
                     'index' => 'loans.index',
@@ -44,23 +50,23 @@ Route::prefix('v1')->group(function () {
                 ]
             );
 
-        Route::post('loans/{loan}/devolution', [App\Http\Controllers\Api\V1\Loan\LoanDevolutionController::class, '__invoke'])
+        Route::post('loans/{loan}/devolution', [LoanDevolutionController::class, '__invoke'])
             ->name('loans.devolution');
 
         Route::prefix('users')->group(function () {
-            Route::get('/', [App\Http\Controllers\Api\V1\User\UserController::class, 'index'])
+            Route::get('/', [UserController::class, 'index'])
                 ->name('users.index');
 
-            Route::get('/{user}', [App\Http\Controllers\Api\V1\User\UserController::class, 'show'])
+            Route::get('/{user}', [UserController::class, 'show'])
                 ->name('users.show');
 
-            Route::put('/{user}', [App\Http\Controllers\Api\V1\User\UserController::class, 'update'])
+            Route::put('/{user}', [UserController::class, 'update'])
                 ->name('users.update');
 
-            Route::put('/{user}/password', [App\Http\Controllers\Api\V1\User\UserController::class, 'updatePassword'])
+            Route::put('/{user}/password', [UserController::class, 'updatePassword'])
                 ->name('users.password.update');
 
-            Route::delete('/{user}', [App\Http\Controllers\Api\V1\User\UserController::class, 'destroy'])
+            Route::delete('/{user}', [UserController::class, 'destroy'])
                 ->name('users.destroy');
         });
 
